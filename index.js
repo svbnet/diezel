bed218693beed48f0abc44b61954dc8c197631d3
Mehavoid <yeer2nado3ooqp1ue@protonmail.com>
2020-05-15 09:34:57 +0300

    Fix filename case

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const MasterKeyProvider = require('./lib/crypto/MasterKeyProvider');
+const MasterKeyProvider = require('./lib/crypto/master-key-provider');
 // Try setting the keys from a keys.json file
 try {
     MasterKeyProvider.setKeys(require('./keys.json'));
@@ -6,8 +6,8 @@ try {
 
 const {MobileClient} = require('./lib/clients/mobileclient');
 const {Cipher, Format} = require('./lib/clients/mediaclient');
-const {SongAsset, LegacyFormat} = require('./lib/content/SongAsset');
-const {ImageUrl, ImageFormat} = require('./lib/content/ImageUrl');
+const {SongAsset, LegacyFormat} = require('./lib/content/songasset');
+const {ImageUrl, ImageFormat} = require('./lib/content/imageurl');
 
 module.exports = {
     clients: {
